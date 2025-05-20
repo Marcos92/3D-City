d9f8ae9246af6b63bf58d922371af503ee49663e
Marcos Rodrigues <marcos.mgbr@gmail.com>
2025-05-20 11:31:05 +0100

    Draggable icons

---
+++ Assets/Scripts/BuildingDraggableIcon.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BuildingDraggableIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Image image;
+    private Transform parent;
+    private RectTransform rectTransform;
+    private RectTransform canvasRectTransform;
+    private Vector2 safePosition;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        parent = transform.parent;
+        rectTransform = GetComponent<RectTransform>();
+        canvasRectTransform = FindFirstObjectByType<Canvas>().GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        safePosition = rectTransform.anchoredPosition;
+        transform.SetParent(canvasRectTransform);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out Vector2 mousePosition
+        );
+
+        rectTransform.anchoredPosition = mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parent);
+        rectTransform.anchoredPosition = safePosition;
+        image.raycastTarget = true;
+    }
+}
